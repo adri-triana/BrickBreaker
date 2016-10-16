eca780317780c0a954ff1d186b2e5d9a3ef2d332
rafael leal <rafael leal>
2016-10-16 01:56:09 -0400

    Basic frame created

--- BrickGame.java
+++ BrickGame.java
@@ -1,15 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author adria
- */
+import javax.swing.JFrame;
+
 public class BrickGame {
-    
-    //test
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Brick Breaker");
+        frame.setSize(600,600);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
     
 }
